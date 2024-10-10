5b32cb14d987099649474bc014e9b4626b146eee
fancykoshka <150930511+fancykoshka@users.noreply.github.com>
2024-10-10 15:06:53 +0300

    Add files via upload

---
+++ PP_2_1_2_Spring_HelloWorld/PP_2_1_2_Spring_HelloWorld-master/PP_2_1_2_Spring_HelloWorld-master/src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Бины helloworld  " + (bean == bean2));
+        System.out.println("Бины cat  " + (cat1 == cat2));
+    }
+}
